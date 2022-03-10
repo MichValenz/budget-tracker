3db36faabb1dc01789abb3048a8e5e57cdbc0e1c
MichValenz <michvalenz27@gmail.com>
2022-03-10 14:09:32 -0600

    added service worker file

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./assets/css/style.css",
+]
+
+self.addEventListener("install", function (e) {});
