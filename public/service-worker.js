2590f34905d7e50f78a1c52fbcfe57205cd4fe6a
MichValenz <michvalenz27@gmail.com>
2022-03-10 22:32:11 -0600

    fixed typo in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "./index.html",
-  "./css/styles.css",
+  "./css/style.css",
   "./js/index.js",
   "./js/idb.js",
   "./manifest.json",
@@ -12,10 +12,10 @@ const FILES_TO_CACHE = [
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
   "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
   "./icons/icon-512x512.png",
   "./icons/icon-384x384.png",
   "./icons/icon-192x192.png",
-  "./icons/icon-152x152.png",
 ];
 
 // Cache resources
